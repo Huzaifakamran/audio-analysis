829772d608963cfe02534f36d82dbb65fed373e6
Huzaifa Kamran <huzaifa.2014@hotmail.com>
2023-12-01 23:36:05 +0500

    12/1/2023

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from pydub import utils,AudioSegment
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+import subprocess
 
 load_dotenv()
 
@@ -32,10 +33,43 @@ def delete_all_files_in_directory(directory_path):
     except Exception as e:
         st.write(f"An error occurred: {e}")
 
+# def detect_silence(path, time):
+#     print('inside')
+#     # Full path to the ffmpeg executable
+#     ffmpeg_path = r'ffmpeg\bin\ffmpeg.exe'  # Replace this with the actual path on your system
+
+#     # Use os.path.join to create the full command
+#     command = f'{ffmpeg_path} -i {path} -af silencedetect=n=-17dB:d={str(time)} -f null -'
+    
+#     out = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+#     stdout, stderr = out.communicate()
+#     s = stdout.decode("utf-8")
+#     k = s.split('[silencedetect @')
+    
+#     if len(k) == 1:
+#         # print(stderr)
+#         return None
+
+#     start, end = [], []
+#     for i in range(1, len(k)):
+#         x = k[i].split(']')[1]
+#         if i % 2 == 0:
+#             x = x.split('|')[0]
+#             x = x.split(':')[1].strip()
+#             end.append(float(x))
+#         else:
+#             x = x.split(':')[1]
+#             x = x.split('size')[0]
+#             x = x.replace('\r', '')
+#             x = x.replace('\n', '').strip()
+#             start.append(float(x))
+#     print(list(zip(start, end)))
+#     return list(zip(start, end))
+
 def convert_audio_to_text(input_path,output_dir,max_size_mb=25):
     with st.spinner('converting audio to the standard format'):
-        print('inside fun')
-        print(input_path.size)
+        # print('inside fun')
+        # print(input_path.size)
         # AudioSegment.converter = "ffmpeg/bin/ffmpeg.exe"                  
         # utils.get_prober_name = get_prober_name
         audio = AudioSegment.from_file(input_path)
@@ -45,37 +79,40 @@ def convert_audio_to_text(input_path,output_dir,max_size_mb=25):
     with st.spinner('splitting audio'):
         # Check file size
         file_size_mb = os.path.getsize(output_path) / (1024 * 1024)  # Size in MB
-        print(file_size_mb)
+        # print(file_size_mb)
+        
         if file_size_mb <= max_size_mb:
-            print('under if')
+            # print('under if')
             parts = [output_path]  # Return a list with the path of the converted .wav file
 
         else:
-            print('under else')
+            # print('under else')
             parts = []
             segment_size_ms = 100000  # Adjust this value based on your needs (10 seconds in this example)
-            print(len(audio))
+    
             for start in range(0, len(audio), segment_size_ms):
                 end = start + segment_size_ms
                 part = audio[start:end]
                 part_path = os.path.join(output_dir, f"{os.path.splitext(os.path.basename(input_path.name))[0]}_{start}-{end}.wav")
                 part.export(part_path, format="wav")
                 parts.append(part_path)
+               
 
-    print('PARTS: ',len(parts))
-    print('PARTS: ',parts)
+    # print('PARTS: ',len(parts))
+    # print('PARTS: ',parts)
+    audio_duration = len(audio) / 60000
     with st.spinner(f'extracting text from {len(parts)} audio files'):
         text = ""
         for i in parts:
-            print(i)
+    
             audio_file = open(i, "rb")
             transcript = client.audio.transcriptions.create(
             model="whisper-1",
             file=audio_file
             )
             text += transcript.text + " "
-        
-        return text
+
+        return text,audio_duration
 
 def main():
     try:
@@ -91,7 +128,7 @@ def main():
             st.audio(st.session_state.audio_file, format="audio/wav", start_time=0)
 
             if st.button("Convert to Text"):
-                TranscriptText = convert_audio_to_text(st.session_state.audio_file,'audios')
+                TranscriptText,audio_duratio = convert_audio_to_text(st.session_state.audio_file,'audios')
                 # converted_files  = convert_audio_to_text(st.session_state.audio_file,'audios')
                 # print("Converted and/or split files:", converted_files)
                 # Add tabs
@@ -103,7 +140,7 @@ def main():
                     st.write(TranscriptText)
                 
                 with duration:
-                    st.write("In Progress...")
+                    st.write(f"The duration of audio file is {audio_duratio} minutes")
                 with st.spinner('analysing the conversation to fetch the required details'):    
                     with details:
                         assistant = client.chat.completions.create(
@@ -112,12 +149,18 @@ def main():
                             {"role": "system", "content":'''You need to give 3 details by checking the below text extracted 
                         from an audio in spanish and in the audio there will be 2 people one is client
                         and the other one is salesman. 
-                        The first detail is "Concept", you need to understand the concept spoken in the audio.
+                             
+                        The first detail is "Concept", you need to do two things here first one is to understand the concept spoken in the audio.
                         For example customer talk about buying a car and talk about speed, 
                         engine, seat, security, etc so you need to understand the converation and 
-                        which part is said by who (client & salesman). 
-                        The second detail is "Client Profile", you need to identify the client profile based on
-                        the following preferences of purchasing: (Trend,Money,Confort, Fidelity,Security and Pride). 
+                        which part is said by who (client & salesman). And the second one is to extract nouns spoken over conversation 
+                        (and number of times it appears) and differates them between client and salesman.
+                             
+                        The second detail is "Client Profile", here you also need to two things, first one is to identify the client profile based on
+                        the following preferences of purchasing: (Trend,Money,Confort, Fidelity,Security and Pride). And the second one is to 
+                        include a percentage distribution of the 6 preferences of client profile for purchaising.
+                        Note: 'Pride' and 'Status' is same thing so use one of them.
+                             
                         And the last detail is to called "Probablity", you need to analyze the conversation based on client profile 
                         and establish a probability of purchasing from 0 to 1.
 
