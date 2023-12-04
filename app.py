7918f75405a0e22e79abd384992356a37dea8fb7
Huzaifa Kamran <huzaifa.2014@hotmail.com>
2023-12-04 15:04:57 +0500

    12/4/2023

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 import subprocess
+import math
 
 load_dotenv()
 
@@ -33,38 +34,44 @@ def delete_all_files_in_directory(directory_path):
     except Exception as e:
         st.write(f"An error occurred: {e}")
 
-# def detect_silence(path, time):
-#     print('inside')
-#     # Full path to the ffmpeg executable
-#     ffmpeg_path = r'ffmpeg\bin\ffmpeg.exe'  # Replace this with the actual path on your system
+def detect_silence(path, time):
+    print('inside')
+    # Full path to the ffmpeg executable
+    ffmpeg_path = r'ffmpeg\bin\ffmpeg.exe'  # Replace this with the actual path on your system
 
-#     # Use os.path.join to create the full command
-#     command = f'{ffmpeg_path} -i {path} -af silencedetect=n=-17dB:d={str(time)} -f null -'
+    # Use os.path.join to create the full command
+    command = f'{ffmpeg_path} -i {path} -af silencedetect=n=-17dB:d={str(time)} -f null -'
     
-#     out = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-#     stdout, stderr = out.communicate()
-#     s = stdout.decode("utf-8")
-#     k = s.split('[silencedetect @')
+    out = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout, stderr = out.communicate()
+    s = stdout.decode("utf-8")
+    k = s.split('[silencedetect @')
     
-#     if len(k) == 1:
-#         # print(stderr)
-#         return None
+    if len(k) == 1:
+        # print(stderr)
+        return None
 
-#     start, end = [], []
-#     for i in range(1, len(k)):
-#         x = k[i].split(']')[1]
-#         if i % 2 == 0:
-#             x = x.split('|')[0]
-#             x = x.split(':')[1].strip()
-#             end.append(float(x))
-#         else:
-#             x = x.split(':')[1]
-#             x = x.split('size')[0]
-#             x = x.replace('\r', '')
-#             x = x.replace('\n', '').strip()
-#             start.append(float(x))
-#     print(list(zip(start, end)))
-#     return list(zip(start, end))
+    start, end = [], []
+    for i in range(1, len(k)):
+        x = k[i].split(']')[1]
+        if i % 2 == 0:
+            x = x.split('|')[0]
+            x = x.split(':')[1].strip()
+            # print(x)
+            end.append(float(x))
+        else:
+            x = x.split(':')[1]
+            x = x.split('size')[0]
+            x = x.replace('\r', '')
+            x = x.replace('\n', '').strip()
+            
+            try:
+                start.append(float(x))
+            except ValueError as e:
+                x=x.split('[')[0]
+                start.append(float(x))
+    # print(list(zip(start, end)))
+    return list(zip(start, end))
 
 def convert_audio_to_text(input_path,output_dir,max_size_mb=25):
     with st.spinner('converting audio to the standard format'):
@@ -100,7 +107,7 @@ def convert_audio_to_text(input_path,output_dir,max_size_mb=25):
 
     # print('PARTS: ',len(parts))
     # print('PARTS: ',parts)
-    audio_duration = len(audio) / 60000
+    audio_duration = round(len(audio) / 60000, 1)
     with st.spinner(f'extracting text from {len(parts)} audio files'):
         text = ""
         for i in parts:
@@ -112,7 +119,7 @@ def convert_audio_to_text(input_path,output_dir,max_size_mb=25):
             )
             text += transcript.text + " "
 
-        return text,audio_duration
+        return text,audio_duration,output_path
 
 def main():
     try:
@@ -128,7 +135,7 @@ def main():
             st.audio(st.session_state.audio_file, format="audio/wav", start_time=0)
 
             if st.button("Convert to Text"):
-                TranscriptText,audio_duratio = convert_audio_to_text(st.session_state.audio_file,'audios')
+                TranscriptText,audio_length,output_path = convert_audio_to_text(st.session_state.audio_file,'audios')
                 # converted_files  = convert_audio_to_text(st.session_state.audio_file,'audios')
                 # print("Converted and/or split files:", converted_files)
                 # Add tabs
@@ -140,33 +147,52 @@ def main():
                     st.write(TranscriptText)
                 
                 with duration:
-                    st.write(f"The duration of audio file is {audio_duratio} minutes")
+                    
+                    silence_list = detect_silence(output_path, time = 4)
+                    start,end = silence_list[0]
+                    start1,end1 = silence_list[-1]
+                    audio_start = math.floor(end)
+                    audio_end = round(end1 / 60, 1)
+
+                    audio_duration = audio_length - round(audio_start / 60, 1)
+
+                    st.write('Audio Details:')
+                    st.write(f"Start: {audio_start}sec")
+                    st.write(f"End: {audio_end}min(s)")
+                    st.write(f"Audio Duration: {audio_duration}min(s)")
+                
+                
                 with st.spinner('analysing the conversation to fetch the required details'):    
                     with details:
                         assistant = client.chat.completions.create(
                         model="gpt-4-1106-preview",
                         messages=[
-                            {"role": "system", "content":'''You need to give 3 details by checking the below text extracted 
+                            {"role": "system", "content":'''You need to give 5 details by checking the below text extracted 
                         from an audio in spanish and in the audio there will be 2 people one is client
                         and the other one is salesman. 
                              
-                        The first detail is "Concept", you need to do two things here first one is to understand the concept spoken in the audio.
+                        The first detail is "Concept", you need to understand the concept spoken in the audio.
                         For example customer talk about buying a car and talk about speed, 
                         engine, seat, security, etc so you need to understand the converation and 
-                        which part is said by who (client & salesman). And the second one is to extract nouns spoken over conversation 
+                        which part is said by who (client & salesman). 
+                        
+                        The second details is "Nouns", you need to extract nouns spoken over conversation 
                         (and number of times it appears) and differates them between client and salesman.
                              
-                        The second detail is "Client Profile", here you also need to two things, first one is to identify the client profile based on
-                        the following preferences of purchasing: (Trend,Money,Confort, Fidelity,Security and Pride). And the second one is to 
-                        include a percentage distribution of the 6 preferences of client profile for purchaising.
-                        Note: 'Pride' and 'Status' is same thing so use one of them.
+                        The third detail is "Client Profile", you need to identify the client profile based on
+                        the following preferences of purchasing: (Trend,Money,Confort, Fidelity,Security and Pride). 
+                        
+                        The fourth detail is "Distribution",you need to include a percentage distribution of the 
+                        6 preferences of client profile for purchaising. And 'Pride' and 'Status' is same thing so use one of them.
                              
-                        And the last detail is to called "Probablity", you need to analyze the conversation based on client profile 
+                        And the fifth detail is to called "Probablity", you need to analyze the conversation based on client profile 
                         and establish a probability of purchasing from 0 to 1.
 
                         In the output each detail should be in the new line like this:
                         Concept:
+                        Nouns:
                         Client Profile:
+                        Distribution:
                         Probablity:
                         
                         Note: Make sure answer will be in spanish'''},
