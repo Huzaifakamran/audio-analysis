9068f7a4d19dfffd078f7a19d667b85099615d11
Muhammad Huzaifa <huzaifa.2014@hotmail.com>
2024-01-03 12:44:36 +0500

    1/3/2024 (3)

--- app.py
+++ app.py
@@ -149,11 +149,7 @@ def convert_audio_to_text(input_path,output_dir,similarity_brands,replacement_wo
             text += transcript.text + " "
 
         for similar, replace in zip(similarity_brands, replacement_words):
-            lower_text = text.lower()
-
-            # Replace similar words while keeping the original casing
-            text = ''.join(replace if lower_text[i:i+len(similar)].lower() == similar.lower() else char
-                        for i, char in enumerate(text))
+            text = text.replace(similar, replace)
 
         return text,audio_duration,output_path
 
@@ -288,13 +284,10 @@ def main():
                         {"role": "user", "content":TranscriptText}]
                         )
                         result = assistant.choices[0].message.content
-                        for similar, replace in zip(similarity_brands, replacement_words):
-                            lower_text = result.lower()
 
-                            # Replace similar words while keeping the original casing
-                            result = ''.join(replace if lower_text[i:i+len(similar)].lower() == similar.lower() else char
-                                        for i, char in enumerate(result))
-                
+                        for similar, replace in zip(similarity_brands, replacement_words):
+                            result = result.replace(similar, replace)
+
                         st.write(result)
 
                 delete_all_files_in_directory('audios')
