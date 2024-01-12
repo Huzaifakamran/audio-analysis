f770c23462ca4c4dc8d9af0d02b8d1fd1b0c7b6e
Muhammad Huzaifa <huzaifa.2014@hotmail.com>
2024-01-12 15:01:43 +0500

    1/12/2024 (11)

--- app.py
+++ app.py
@@ -156,7 +156,7 @@ def convert_audio_to_text(input_path,output_dir,similarity_brands,replacement_wo
             prompt = f"{brand_list}",
             file=audio_file,
             language='es',
-            temperature=0.7
+            temperature=0.6
             )
             text += transcript.text + " "
 
